1f02e0f17d245584c5ce4cfcb82a611bbc268026
Diniz <abessa-m@c1r2s7.42porto.com>
2024-10-22 19:32:46 +0100

    initial isalpha

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,7 +6,33 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 13:54:33 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/10/22 13:54:34 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/10/22 19:29:34 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// 1 is TRUE
+// 0 is FALSE
+char	ft_isalpha(char c)
+{
+	if (ft_isupper(c) ||  ft_islower(c))
+		return (c);
+	else
+		return (0);
+}
+
+// Checks if the char is an uppercase letter
+static int	ft_isupper(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c);
+	else
+		return (0);
+}
+
+static int	ft_islower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c);
+	else
+		return (0);
+}
