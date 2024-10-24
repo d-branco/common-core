1a0aa63710152f2838a154df162dd9be20c9ede2
gris <185072671+d-branco@users.noreply.github.com>
2024-10-24 17:35:55 +0100

    isascii sem test

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isacii.c                                        :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:49:01 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/10/24 16:51:57 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/10/24 17:31:20 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //checks whether c is a 7-bit unsigned char value that  fits  into
 //	the ASCII character set.
 
+// checks if the character is between 0 and 127 inclusive.
 int	ft_isascii(int c)
 {
-	q
+	if (c >= 0 && c <= 127)
+		return (1);
+	return (0);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 08:10:20 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/10/24 13:07:14 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/10/24 17:29:41 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
+int	ft_isascii(int c);
 
 #endif
