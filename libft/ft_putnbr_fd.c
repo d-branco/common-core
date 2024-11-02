4cd8e459894606840627e10038ee66b4f746f1ea
d-cluster <abessa-m@c1r2s7.42porto.com>
2024-11-02 12:11:56 +0000

    putnbr

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 10:39:41 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/11/02 11:13:31 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/11/02 11:40:34 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,12 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n < 0)
-		n = -n
-	if (n > 10)
+		n = -n;
+	if (n < 10)
+		ft_putchar_fd((n + '0'), fd);
+	else
 	{
-		
+		ft_putnbr_fd((n / 10), fd);
+		ft_putnbr_fd((n % 10), fd);
 	}
 }
