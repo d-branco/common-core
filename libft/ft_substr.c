22a9cd772c70789e68ba98231895bb76b2ec732a
gris <185072671+d-branco@users.noreply.github.com>
2024-11-02 09:00:53 +0000

    new approach

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 11:27:48 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/11/01 12:38:04 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/11/02 08:57:28 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	ft_strdup(&s[start])
+	// COm calloc Cria uma nova string com comprimento len
+	// Copia com strlcpy
+	ft_strdup(&s[start]);
 }
