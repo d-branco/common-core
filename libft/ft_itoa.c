8ebcae51c2c09c01775e4b63a186d20d5da6e295
gris <185072671+d-branco@users.noreply.github.com>
2024-11-04 10:41:07 +0000

    itoa initial

---
+++ libft/ft_itoa.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 09:14:26 by abessa-m          #+#    #+#             */
+/*   Updated: 2024/11/04 10:40:56 by abessa-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//	Function name
+//		ft_itoa
+//	Prototype
+//		char *ft_itoa(int n);
+//	Turn in files
+//		-
+//	Parameters
+//		n: the integer to convert.
+//	Return value
+//		The string representing the integer.
+//		NULL if the allocation fails.
+//	External functs.
+//		malloc
+//	Description
+//		Allocates (with malloc(3)) and returns a string representing the
+//	integer received as an argument.
+//		Negative numbers must be handled.
+
+#include "libft.h"
+//	malloc(), 
+
+char	*ft_itoa(int n)
+{
+	
+}
