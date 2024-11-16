4a45cf5229590a8ac081066c48ddbe4dc5b47a92
d-cluster <abessa-m@c1r2s7.42porto.com>
2024-11-16 18:29:21 +0000

    read()

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,21 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 09:26:14 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/11/15 09:30:25 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/11/16 12:18:38 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+// defines BUFFER_SIZE to 42, in case the flag "-D BUFFER_SIZE" is not used
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
 # include <stdlib.h>	// malloc(), free()
 # include <unistd.h>	// read()
+
+
 
 #endif
