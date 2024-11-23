8404f22d8e6903e10fb891e3ff24848ea8c3a682
gris <185072671+d-branco@users.noreply.github.com>
2024-11-23 16:41:03 +0000

    pre-definition

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 09:26:14 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/11/19 08:45:58 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/11/25 14:48:18 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-// defines BUFFER_SIZE to 42, in case the flag "-D BUFFER_SIZE" is not used
+// pre-defines BUFFER_SIZE to 42
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
